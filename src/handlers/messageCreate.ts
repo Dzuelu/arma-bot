468bfa445756196dc7e7e747800d22c3fe2f7989
Dzuelu <Dzuelu@users.noreply.github.com>
2022-03-05 12:02:13 -0500

    remove iSleep

--- src/handlers/messageCreate.ts
+++ src/handlers/messageCreate.ts
@@ -1,10 +1,10 @@
 import { Message } from 'discord.js';
-import { anyContent, iSleep } from 'reactions';
+import { anyContent } from 'reactions';
 import { chance } from 'utils';
 
 export const messageCreate = async (message: Message<boolean>): Promise<void> => {
   // console.log('messageCreate', message);
 
-  chance(0.001, () => iSleep(message));
+  // chance(0.5, () => iSleep(message));
   anyContent(message);
 };
